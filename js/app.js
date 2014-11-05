f31da720ab4b142dc16b5ac2ff4ac49171684f9b
Harsh <harshmathur.1990@gmail.com>
2014-11-05 10:57:25 +0530

    menu bar

--- js/app.js
+++ js/app.js
@@ -4,8 +4,25 @@
 
 	app.controller('WindowController', function() {
 		this.text = "Old Text";
-
+		this.main = false;
+		
 		this.change = function() {
 			this.text = "New Text";
 		};
+		
+		this.setMain = function() {
+			this.main = false;
+		};
+		
+		this.getMain = function() {
+			return this.main;
+		};
+		
+		this.getUMain = function() {
+			return !this.main;
+		};
+		
+		this.setType = function(type) {
+			this.main = true;
+		}
 	});
