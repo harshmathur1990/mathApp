d408974c91b583f81c7aa388f6032d67961a18bd
Harsh <harshmathur.1990@gmail.com>
2014-11-06 15:45:38 +0530

    generated numbers

--- js/app.js
+++ js/app.js
@@ -5,7 +5,12 @@
 	app.controller('WindowController', function() {
 		this.text = "Old Text";
 		this.main = false;
-		
+		this.num1 = 0;
+		this.num2 = 0;
+		this.result = 0;
+		this.num1arr = new Array(6);
+		this.num2arr = new Array(6);
+
 		this.change = function() {
 			this.text = "New Text";
 		};
@@ -24,5 +29,50 @@
 		
 		this.setType = function(type) {
 			this.main = true;
+			this.generateNum();
+			if (type == 1) {
+				this.result = this.num1 + this.num2;
+			}
+			
+			else if (type == 2) {
+				this.result = this.num1 - this.num2;
+			}
+			
+			else if (type == 3) {
+				this.result = this.num1 * this.num2;
+				while (this.result >= 1000000) {
+					this.generateNum();
+					this.result = this.num1 * this.num2;
+				}
+			}
+			else {
+				this.result = this.num1/ this.num2;
+			}
+			
+			i = 0;
+			while (this.num1 > 0) {
+				this.num1arr[i] = this.num1%10;
+				console.log("this.num1arr["+i+"] = "+ this.num1arr[i]);
+				this.num1 = Math.floor(this.num1 / 10);
+				console.log("this.num1 = " + this.num1);
+				i++;
+			}
+			i = 0;
+			while (this.num2 > 0) {
+				this.num2arr[i] = this.num2%10;
+				console.log("this.num2arr["+i+"] = " + this.num2arr[i]);
+				this.num2 = Math.floor(this.num2 / 10);
+				console.log("this.num2 = " + this.num2);
+				i++;
+			}
+		};
+		
+		this.generateNum = function() {
+			a = Math.floor(Math.random() * (100000 - 1)) + 1;
+			b = Math.floor(Math.random() * (100000 - 1)) + 1;
+			console.log ("A : " + a);
+			console.log("B : " + b);
+			this.num1 = a>b?a:b;
+			this.num2 = a<b?a:b;
 		}
 	});
